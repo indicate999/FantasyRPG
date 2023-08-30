5effa72e399fd907f813e2830d12e9b55c03e747
indicate999 <inovitskiy13@gmail.com>
2023-08-30 10:28:50 +0200

    clear3

--- FantasyRPG/Assets/Scripts/Mob/MobCreator.cs
+++ FantasyRPG/Assets/Scripts/Mob/MobCreator.cs
@@ -10,31 +10,27 @@ public class MobCreator : MonoBehaviour
 
     private void Start()
     {
+        CreatMob();
+    }
+
+    private void CreatMob()
+    {
         //GameObject mobObject = Instantiate(SpyPrefab);
         GameObject mobObject = Instantiate(GolemPrefab);
-
-        //MobAnimatorController mobAnimator = mobObject.AddComponent<MobAnimatorController>();
-
-        //mobAnimator.SetWalkAnimation();
 
         MobController mobController = mobObject.AddComponent<MobController>();
         MobAnimatorController mobAnimator = mobObject.GetComponent<MobAnimatorController>();
         Rigidbody mobRigidbody = mobObject.GetComponent<Rigidbody>();
 
-        //mobController.Init(new StandStillWalkBehavior(mobObject.transform, transform.position, 270f));
-       //var walkBehavior = new StandStillWalkBehavior(mobObject.transform, transform.position, 270f);
+        //var walkBehavior = new StandStillWalkBehavior(mobObject.transform, transform.position, 270f);
         var walkBehavior = new StraightPatrolWalkBehavior
             (mobAnimator, mobRigidbody, mobObject.transform, transform.position, 270f, 25f, 5f, 1f);
 
-        //var attackBehavior = new StandStillAttackBehavior(mobAnimator, PlayerTransform, mobObject.transform, 5);
-
+        //var moveBehavior = new StandStillMoveBehavior(mobAnimator, PlayerTransform, mobObject.transform, 5);
         var moveBehavior = new GoToPlayerMoveBehavior(mobAnimator, mobRigidbody, PlayerTransform, mobObject.transform, 5f, 5f);
 
         var attackBehavior = new MeleeAttackBehavior(mobAnimator);
 
         mobController.Init(new MobStateMachine(walkBehavior, moveBehavior, attackBehavior, PlayerTransform, mobObject.transform, 8, 3));
-
-
     }
-
 }
